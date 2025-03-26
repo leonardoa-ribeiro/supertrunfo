120c3e1bbc67cb5fd87202e8c37653548dc2295a
leonardoa-ribeiro <158476292+leonardoa-ribeiro@users.noreply.github.com>
2025-03-26 00:04:24 +0000

    removendo comentários

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 typedef struct {
-    char estado[3];  // O estado é representado por duas letras (ex: "SP")
-    char cidade[30]; // Nome da cidade
+    char estado[3]; 
+    char cidade[30]; 
     int populacao;
-    char codigoCarta[5];  // Código da carta (ex: "A01", "B02")
+    char codigoCarta[5];  
     float area;
     float pib;
     int pontosTuristicos;
@@ -16,12 +16,12 @@ int main () {
     // Entrada para a primeira carta
     printf("\nDigite os dados da carta 01\n");
     printf("Estado (ex: SP): ");
-    scanf("%s", carta1.estado);  // Leitura do estado (2 caracteres)
+    scanf("%s", carta1.estado);  
     printf("Código da carta (ex: A01): ");
-    scanf("%s", carta1.codigoCarta);  // Leitura do código da carta
+    scanf("%s", carta1.codigoCarta);  
     printf("Nome da cidade: ");
-    getchar();  // Limpa o buffer do \n deixado por scanf
-    fgets(carta1.cidade, sizeof(carta1.cidade), stdin);  // Leitura da cidade
+    getchar();  
+    fgets(carta1.cidade, sizeof(carta1.cidade), stdin);  
     printf("População: ");
     scanf("%d", &carta1.populacao);
     printf("Área (em Km²): ");
@@ -34,12 +34,12 @@ int main () {
     // Entrada para a segunda carta
     printf("\nDigite os dados da carta 02\n");
     printf("Estado (ex: SP): ");
-    scanf("%s", carta2.estado);  // Leitura do estado (2 caracteres)
+    scanf("%s", carta2.estado);  
     printf("Código da carta (ex: B02): ");
-    scanf("%s", carta2.codigoCarta);  // Leitura do código da carta
+    scanf("%s", carta2.codigoCarta); 
     printf("Nome da cidade: ");
-    getchar();  // Limpa o buffer do \n
-    fgets(carta2.cidade, sizeof(carta2.cidade), stdin);  // Leitura da cidade
+    getchar();  
+    fgets(carta2.cidade, sizeof(carta2.cidade), stdin);  
     printf("População: ");
     scanf("%d", &carta2.populacao);
     printf("Área (em Km²): ");
@@ -56,7 +56,7 @@ int main () {
     printf("\nDados da carta 01\n");
     printf("Código da carta: %s\n", carta1.codigoCarta);
     printf("Estado: %s\n", carta1.estado);
-    printf("Cidade: %s", carta1.cidade);  // Não é necessário o \n adicional após fgets
+    printf("Cidade: %s", carta1.cidade);  
     printf("População: %d\n", carta1.populacao);
     printf("Área: %.2f Km²\n", carta1.area);
     printf("PIB: %.2f\n", carta1.pib);
